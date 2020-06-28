0871f5a7dc401348683e9f39d5778a790d4f5bd8
mkumarkumar <mohanjuly100@yahoo.com>
2020-06-28 16:27:25 +0530

    Create Janya1.java

---
+++ Janya1.java
@@ -0,0 +1,24 @@
+
+public class Janya1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		int x, y, sum, diff, prod, remin ;
+		x=7;
+		y=4;
+		sum = x+y;
+		diff = x-y;
+		prod = x * y;
+		remin = x % y ;
+		 
+		System.out.println("Sum of two number is " + sum);
+		System.out.println("Difference of two number is " + diff);
+		System.out.println("Product of two number is " + prod);
+		System.out.println("Division of two number is " + remin);
+		System.out.println("Congratulations Chikku on the first Java Programing !!! ");
+
+	}
+
+}
